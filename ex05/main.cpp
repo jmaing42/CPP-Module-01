395aa62db25ccfa7013a47a32a4e6dd84107df18
Juyeong Maing <mjy9088@naver.com>
2022-10-14 23:19:50 +0900

    add ex05

---
+++ ex05/main.cpp
@@ -0,0 +1,11 @@
+
+#include "Harl.hpp"
+
+int main() {
+  Harl harl;
+  harl.complain("Any string");
+  harl.complain("debug");
+  harl.complain("info");
+  harl.complain("warn");
+  harl.complain("error");
+}

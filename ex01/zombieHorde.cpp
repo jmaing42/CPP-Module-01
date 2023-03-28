e834a524b20ebbb3655ecd9af37f827851cdd35c
Juyeong Maing <mjy9088@naver.com>
2023-03-28 13:12:33 +0900

    fix potential memory leak

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -5,8 +5,13 @@
 Zombie *zombieHorde(int N, std::string name) {
   Zombie initial(name);
   Zombie *zombies = new Zombie[N];
-  for (std::size_t i = 0; i < (unsigned int)N; i++) {
-    zombies[i] = initial;
+  try {
+    for (std::size_t i = 0; i < (unsigned int)N; i++) {
+      zombies[i] = initial;
+    }
+    return zombies;
+  } catch (std::exception &e) {
+    delete[] zombies;
+    throw e;
   }
-  return zombies;
 }
